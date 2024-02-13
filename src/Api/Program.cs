37f92242673319f7e6e506bae7e5209d09628580
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2024-02-13 12:59:44 +0300

    Adds Persistence using SQLite

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,12 @@
+using Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 // Add services to the container.
+
+builder.Services.AddInfrastructure(config);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/Domain/Common/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Common.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; } = null;
+    }
+}
